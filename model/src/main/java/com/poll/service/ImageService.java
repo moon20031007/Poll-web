f11a38548b6277a5a5ffe144af99b43563c8aaee
ld <2636064351@qq.com>
2024-12-03 13:51:49 +0800

    an example of poll creation in example.html

--- model/src/main/java/com/poll/service/ImageService.java
+++ model/src/main/java/com/poll/service/ImageService.java
@@ -1,4 +1,8 @@
 package com.poll.service;
 
+import com.poll.pojo.Image;
+
 public interface ImageService {
+
+    void insert(Image image);
 }
--- model/src/main/java/com/poll/service/impl/ImageServiceImpl.java
+++ model/src/main/java/com/poll/service/impl/ImageServiceImpl.java
@@ -1,8 +1,22 @@
 package com.poll.service.impl;
 
+import com.poll.mapper.ImageMapper;
+import com.poll.pojo.Image;
 import com.poll.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ImageServiceImpl implements ImageService {
+
+    private final ImageMapper imageMapper;
+
+    public ImageServiceImpl(ImageMapper imageMapper) {
+        this.imageMapper = imageMapper;
+    }
+
+    @Override
+    public void insert(Image image) {
+        imageMapper.insert(image);
+    }
 }
